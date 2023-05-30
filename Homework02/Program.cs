e41222943c3e781fd20be2ddd20018b60a1d3af6
mashkamashka <romanova98ma@mail.ru>
2023-05-30 17:56:30 +0300

    first commit

---
+++ Homework02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+if (A>B)
+{
+    Console.Write($"max = {A}");
+}
+ 
+if (B>A)
+ {
+    Console.Write($"max = {B}");
+ }
+
+if (A==B)
+{
+    Console.Write("Числа равны");
+}
