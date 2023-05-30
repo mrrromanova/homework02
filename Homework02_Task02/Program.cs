e63b66baf594e89cddacdee17009e4e80b96c8f6
mashkamashka <romanova98ma@mail.ru>
2023-05-30 18:12:31 +0300

    second task

---
+++ Homework02_Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int C = int.Parse(Console.ReadLine());
+int max = A;
+if (B>max)
+{
+    max=B;
+}
+
+if (C>max)
+{
+    max=C;
+}
+
+Console.Write($"max={max}");
